b8c669049a274100c68d51408ecf79ce758c1ecb
Ryan Womack <rwomack@rutgers.edu>
2026-06-24 13:51:44 -0400

    regression modified

--- mtcars_regression.R
+++ mtcars_regression.R
@@ -1,13 +1,21 @@
+# load data
+
 data(mtcars)
 ?mtcars
 
+# correlation
+
 cor(disp,hp)
 cor(disp,mpg)
+
+# single variable regression
 
 lm(mpg~disp)
 regoutput <- lm(mpg~disp)
 summary(regoutput)
 plot(regoutput)
+
+# multiple variable regression
 
 lm(mpg~disp+am+wt+gear)
 regoutput2 <- lm(mpg~disp+am+wt+gear)
@@ -17,5 +25,30 @@ lm(mpg~disp+wt)
 regoutput3 <- lm(mpg~disp+wt)
 summary(regoutput3)
 
+# visualize
+
 library(tidyverse)
 ggplot(mtcars, aes(mpg,wt)) + geom_point() + geom_smooth(method='lm')
+
+# visual refinements
+
+library(RColorBrewer)
+library(ggthemes)
+
+mtcars$cylfactor <- as.factor(mtcars$cyl)
+
+attach(mtcars)
+
+ggplot(mtcars, aes(mpg,wt)) + geom_point(color=cylfactor) + 
+         geom_smooth(method='lm') + 
+         labs(title="Predictors of gas mileage", y="weight", x="miles per gallon") +
+         theme_dark() +
+  scale_color_brewer(palette = "Dark2")
+    
+# or 
+
+ggplot(mtcars, aes(mpg,wt, color=cylfactor)) + geom_point() + 
+  geom_smooth(method='lm') + 
+  labs(title="Predictors of gas mileage", y="weight", x="miles per gallon") +
+  theme_dark() +
+  scale_color_brewer(palette = "Set2")
